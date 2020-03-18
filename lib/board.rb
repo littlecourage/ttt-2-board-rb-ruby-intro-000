fb2bb7943ba7a24dcd3d385049106c0dfd0c461e
Christine Adams <christine.adams21@gmail.com>
2020-03-18 20:11:04 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
 # Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Position 1: Upper Row Left
+board[0]
+
+# Position 2: Upper Row Center
+board[1]
+
+# Position 3: Upper Row Right
+board[2]
+
+# Position 4: Middle Row Left
+board[3]
+
+# Position 5: Middle Row Center
+board[4]
+
+# Position 6: Middle Row Right
+board[5]
+
+# Position 7: Bottom Row Left
+board[6]
+
+#Position 8: Bottom Row Center
+board[7]
+
+#Position 9: Bottom Row Right
+board[8]
